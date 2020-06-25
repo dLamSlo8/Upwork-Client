113a21a7e48ec8c34b485264d10885fb6a9289eb
Derek Lam <dereklam67@gmail.com>
2020-06-25 15:23:21 -0700

    new changes

--- index.js
+++ index.js
@@ -1,54 +1,8 @@
 const handleNav = () => {
-    console.log("loaded!");
-    let selectedPage = 0;
-
-    const pagesNav = $("ul#nav > li");
-    const pages = $("div[id^='page']");
-
-    pagesNav.each(function(idx) {
-        let that = this;
-        $(this).click(function() {
-            pages[selectedPage].classList.remove("page-selected");
-            pages[idx].classList.add("page-selected");
-
-            pagesNav[selectedPage].classList.remove("book-nav-selected");
-            $(that).addClass("book-nav-selected");
-
-            selectedPage = idx;
-
-            $("#nav-page").text(`Page ${selectedPage + 1}`);
-        });
+    $(".top-nav-hamburger").click(function() {
+        console.log("clicked");
+        $(".top-nav ul").slideToggle("slow", function() {});
     })
-
-    $("#nav-back").click(function() {
-        if (selectedPage !== 0) {
-            pages[selectedPage].classList.remove("page-selected");
-            pages[selectedPage - 1].classList.add("page-selected");
-
-            pagesNav[selectedPage].classList.remove("book-nav-selected");
-            pagesNav[selectedPage - 1].classList.add("book-nav-selected");
-            
-            selectedPage -= 1;
-
-            $("#nav-page").text(`Page ${selectedPage + 1}`);
-        }
-    });
-
-    $("#nav-forward").click(function() {
-        if (selectedPage !== pages.length - 1) {
-            pages[selectedPage].classList.remove("page-selected");
-            pages[selectedPage + 1].classList.add("page-selected");
-
-            pagesNav[selectedPage].classList.remove("book-nav-selected");
-            pagesNav[selectedPage + 1].classList.add("book-nav-selected");
-            
-            selectedPage += 1;
-            
-            $("#nav-page").text(`Page ${selectedPage + 1}`);
-        }
-    });
-
-
 };
 
 window.onload = (event) => {
