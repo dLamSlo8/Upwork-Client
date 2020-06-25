ecc0fc7fce9ac8274533e5a632c66fd4d5894d97
Derek Lam <dereklam67@gmail.com>
2020-06-25 15:46:31 -0700

    new changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const handleNav = () => {
+    $(".poppy-image").fadeIn("slow", function() {});
     $(".top-nav-hamburger").click(function() {
         console.log("clicked");
         $(".top-nav ul").slideToggle("slow", function() {});
